41262ccae3540ce8e95d35768384c14e6c77e7e5
Sébastien Gruhier <sebastien.gruhier@xilinus.com>
2014-04-26 16:52:16 +0200

    jQuery noConflict fix #9

--- dist/typeahead-addresspicker.js
+++ dist/typeahead-addresspicker.js
@@ -3,229 +3,230 @@
     __hasProp = {}.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
-  this.AddressPickerResult = (function() {
-    function AddressPickerResult(placeResult, fromReverseGeocoding) {
-      this.placeResult = placeResult;
-      this.fromReverseGeocoding = fromReverseGeocoding != null ? fromReverseGeocoding : false;
-      this.latitude = this.placeResult.geometry.location.lat();
-      this.longitude = this.placeResult.geometry.location.lng();
-    }
-
-    AddressPickerResult.prototype.addressTypes = function() {
-      var component, type, types, _i, _j, _len, _len1, _ref, _ref1;
-      types = [];
-      _ref = this.addressComponents();
-      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-        component = _ref[_i];
-        _ref1 = component.types;
-        for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
-          type = _ref1[_j];
-          if (types.indexOf(type) === -1) {
-            types.push(type);
-          }
-        }
+  (function($) {
+    this.AddressPickerResult = (function() {
+      function AddressPickerResult(placeResult, fromReverseGeocoding) {
+        this.placeResult = placeResult;
+        this.fromReverseGeocoding = fromReverseGeocoding != null ? fromReverseGeocoding : false;
+        this.latitude = this.placeResult.geometry.location.lat();
+        this.longitude = this.placeResult.geometry.location.lng();
       }
-      return types;
-    };
 
-    AddressPickerResult.prototype.addressComponents = function() {
-      return this.placeResult.address_components || [];
-    };
-
-    AddressPickerResult.prototype.address = function() {
-      return this.placeResult.formatted_address;
-    };
-
-    AddressPickerResult.prototype.nameForType = function(type, shortName) {
-      var component, _i, _len, _ref;
-      if (shortName == null) {
-        shortName = false;
-      }
-      _ref = this.addressComponents();
-      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-        component = _ref[_i];
-        if (component.types.indexOf(type) !== -1) {
-          return (shortName ? component.short_name : component.long_name);
-        }
-      }
-      return null;
-    };
-
-    AddressPickerResult.prototype.lat = function() {
-      return this.latitude;
-    };
-
-    AddressPickerResult.prototype.lng = function() {
-      return this.longitude;
-    };
-
-    AddressPickerResult.prototype.setLatLng = function(latitude, longitude) {
-      this.latitude = latitude;
-      this.longitude = longitude;
-    };
-
-    AddressPickerResult.prototype.isAccurate = function() {
-      return !this.placeResult.geometry.viewport;
-    };
-
-    AddressPickerResult.prototype.isReverseGeocoding = function() {
-      return this.fromReverseGeocoding;
-    };
-
-    return AddressPickerResult;
-
-  })();
-
-  this.AddressPicker = (function(_super) {
-    __extends(AddressPicker, _super);
-
-    function AddressPicker(options) {
-      if (options == null) {
-        options = {};
-      }
-      this.markerDragged = __bind(this.markerDragged, this);
-      this.updateBoundsForPlace = __bind(this.updateBoundsForPlace, this);
-      this.updateMap = __bind(this.updateMap, this);
-      this.options = $.extend({
-        local: [],
-        datumTokenizer: function(d) {
-          return Bloodhound.tokenizers.whitespace(d.num);
-        },
-        queryTokenizer: Bloodhound.tokenizers.whitespace,
-        autocompleteService: {
-          types: ["geocode"]
-        },
-        zoomForLocation: 16,
-        reverseGeocoding: false
-      }, options);
-      AddressPicker.__super__.constructor.call(this, this.options);
-      if (this.options.map) {
-        this.initMap();
-      }
-      this.placeService = new google.maps.places.PlacesService(document.createElement('div'));
-    }
-
-    AddressPicker.prototype.bindDefaultTypeaheadEvent = function(typeahead) {
-      typeahead.bind("typeahead:selected", this.updateMap);
-      return typeahead.bind("typeahead:cursorchanged", this.updateMap);
-    };
-
-    AddressPicker.prototype.initMap = function() {
-      var markerOptions, _ref, _ref1;
-      if ((_ref = this.options) != null ? (_ref1 = _ref.map) != null ? _ref1.gmap : void 0 : void 0) {
-        this.map = this.options.map.gmap;
-      } else {
-        this.mapOptions = $.extend({
-          zoom: 3,
-          center: new google.maps.LatLng(0, 0),
-          mapTypeId: google.maps.MapTypeId.ROADMAP,
-          boundsForLocation: this.updateBoundsForPlace
-        }, this.options.map);
-        this.map = new google.maps.Map($(this.mapOptions.id)[0], this.mapOptions);
-      }
-      this.lastResult = null;
-      markerOptions = $.extend({
-        draggable: true,
-        visible: false,
-        position: this.map.getCenter(),
-        map: this.map
-      }, this.options.marker || {});
-      this.marker = new google.maps.Marker(markerOptions);
-      if (markerOptions.draggable) {
-        return google.maps.event.addListener(this.marker, 'dragend', this.markerDragged);
-      }
-    };
-
-    AddressPicker.prototype.get = function(query, cb) {
-      var service;
-      service = new google.maps.places.AutocompleteService();
-      this.options.autocompleteService.input = query;
-      return service.getPlacePredictions(this.options.autocompleteService, function(predictions) {
-        var data, suggestion;
-        data = (function() {
-          var _i, _len, _results;
-          _results = [];
-          for (_i = 0, _len = predictions.length; _i < _len; _i++) {
-            suggestion = predictions[_i];
-            _results.push(suggestion);
-          }
-          return _results;
-        })();
-        return cb(data);
-      });
-    };
-
-    AddressPicker.prototype.updateMap = function(event, place) {
-      return this.placeService.getDetails(place, (function(_this) {
-        return function(response) {
-          var _ref;
-          _this.lastResult = new AddressPickerResult(response);
-          if (_this.marker) {
-            _this.marker.setPosition(response.geometry.location);
-            _this.marker.setVisible(true);
-          }
-          if (_this.map) {
-            if ((_ref = _this.mapOptions) != null) {
-              _ref.boundsForLocation(response);
+      AddressPickerResult.prototype.addressTypes = function() {
+        var component, type, types, _i, _j, _len, _len1, _ref, _ref1;
+        types = [];
+        _ref = this.addressComponents();
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          component = _ref[_i];
+          _ref1 = component.types;
+          for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
+            type = _ref1[_j];
+            if (types.indexOf(type) === -1) {
+              types.push(type);
             }
           }
-          return $(_this).trigger('addresspicker:selected', _this.lastResult);
-        };
-      })(this));
-    };
+        }
+        return types;
+      };
 
-    AddressPicker.prototype.updateBoundsForPlace = function(response) {
-      if (response.geometry.viewport) {
-        return this.map.fitBounds(response.geometry.viewport);
-      } else {
-        this.map.setCenter(response.geometry.location);
-        return this.map.setZoom(this.options.zoomForLocation);
+      AddressPickerResult.prototype.addressComponents = function() {
+        return this.placeResult.address_components || [];
+      };
+
+      AddressPickerResult.prototype.address = function() {
+        return this.placeResult.formatted_address;
+      };
+
+      AddressPickerResult.prototype.nameForType = function(type, shortName) {
+        var component, _i, _len, _ref;
+        if (shortName == null) {
+          shortName = false;
+        }
+        _ref = this.addressComponents();
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          component = _ref[_i];
+          if (component.types.indexOf(type) !== -1) {
+            return (shortName ? component.short_name : component.long_name);
+          }
+        }
+        return null;
+      };
+
+      AddressPickerResult.prototype.lat = function() {
+        return this.latitude;
+      };
+
+      AddressPickerResult.prototype.lng = function() {
+        return this.longitude;
+      };
+
+      AddressPickerResult.prototype.setLatLng = function(latitude, longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+      };
+
+      AddressPickerResult.prototype.isAccurate = function() {
+        return !this.placeResult.geometry.viewport;
+      };
+
+      AddressPickerResult.prototype.isReverseGeocoding = function() {
+        return this.fromReverseGeocoding;
+      };
+
+      return AddressPickerResult;
+
+    })();
+    return this.AddressPicker = (function(_super) {
+      __extends(AddressPicker, _super);
+
+      function AddressPicker(options) {
+        if (options == null) {
+          options = {};
+        }
+        this.markerDragged = __bind(this.markerDragged, this);
+        this.updateBoundsForPlace = __bind(this.updateBoundsForPlace, this);
+        this.updateMap = __bind(this.updateMap, this);
+        this.options = $.extend({
+          local: [],
+          datumTokenizer: function(d) {
+            return Bloodhound.tokenizers.whitespace(d.num);
+          },
+          queryTokenizer: Bloodhound.tokenizers.whitespace,
+          autocompleteService: {
+            types: ["geocode"]
+          },
+          zoomForLocation: 16,
+          reverseGeocoding: false
+        }, options);
+        AddressPicker.__super__.constructor.call(this, this.options);
+        if (this.options.map) {
+          this.initMap();
+        }
+        this.placeService = new google.maps.places.PlacesService(document.createElement('div'));
       }
-    };
 
-    AddressPicker.prototype.markerDragged = function() {
-      if (this.options.reverseGeocoding) {
-        return this.reverseGeocode(this.marker.getPosition());
-      } else {
-        if (this.lastResult) {
-          this.lastResult.setLatLng(this.marker.getPosition().lat(), this.marker.getPosition().lng());
+      AddressPicker.prototype.bindDefaultTypeaheadEvent = function(typeahead) {
+        typeahead.bind("typeahead:selected", this.updateMap);
+        return typeahead.bind("typeahead:cursorchanged", this.updateMap);
+      };
+
+      AddressPicker.prototype.initMap = function() {
+        var markerOptions, _ref, _ref1;
+        if ((_ref = this.options) != null ? (_ref1 = _ref.map) != null ? _ref1.gmap : void 0 : void 0) {
+          this.map = this.options.map.gmap;
         } else {
-          this.lastResult = new AddressPickerResult({
-            geometry: {
-              location: this.marker.getPosition()
-            }
-          });
+          this.mapOptions = $.extend({
+            zoom: 3,
+            center: new google.maps.LatLng(0, 0),
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            boundsForLocation: this.updateBoundsForPlace
+          }, this.options.map);
+          this.map = new google.maps.Map($(this.mapOptions.id)[0], this.mapOptions);
         }
-        return $(this).trigger('addresspicker:selected', this.lastResult);
-      }
-    };
+        this.lastResult = null;
+        markerOptions = $.extend({
+          draggable: true,
+          visible: false,
+          position: this.map.getCenter(),
+          map: this.map
+        }, this.options.marker || {});
+        this.marker = new google.maps.Marker(markerOptions);
+        if (markerOptions.draggable) {
+          return google.maps.event.addListener(this.marker, 'dragend', this.markerDragged);
+        }
+      };
 
-    AddressPicker.prototype.reverseGeocode = function(position) {
-      if (this.geocoder == null) {
-        this.geocoder = new google.maps.Geocoder();
-      }
-      return this.geocoder.geocode({
-        location: position
-      }, (function(_this) {
-        return function(results) {
-          if (results && results.length > 0) {
-            _this.lastResult = new AddressPickerResult(results[0], true);
+      AddressPicker.prototype.get = function(query, cb) {
+        var service;
+        service = new google.maps.places.AutocompleteService();
+        this.options.autocompleteService.input = query;
+        return service.getPlacePredictions(this.options.autocompleteService, function(predictions) {
+          var data, suggestion;
+          data = (function() {
+            var _i, _len, _results;
+            _results = [];
+            for (_i = 0, _len = predictions.length; _i < _len; _i++) {
+              suggestion = predictions[_i];
+              _results.push(suggestion);
+            }
+            return _results;
+          })();
+          return cb(data);
+        });
+      };
+
+      AddressPicker.prototype.updateMap = function(event, place) {
+        return this.placeService.getDetails(place, (function(_this) {
+          return function(response) {
+            var _ref;
+            _this.lastResult = new AddressPickerResult(response);
+            if (_this.marker) {
+              _this.marker.setPosition(response.geometry.location);
+              _this.marker.setVisible(true);
+            }
+            if (_this.map) {
+              if ((_ref = _this.mapOptions) != null) {
+                _ref.boundsForLocation(response);
+              }
+            }
             return $(_this).trigger('addresspicker:selected', _this.lastResult);
+          };
+        })(this));
+      };
+
+      AddressPicker.prototype.updateBoundsForPlace = function(response) {
+        if (response.geometry.viewport) {
+          return this.map.fitBounds(response.geometry.viewport);
+        } else {
+          this.map.setCenter(response.geometry.location);
+          return this.map.setZoom(this.options.zoomForLocation);
+        }
+      };
+
+      AddressPicker.prototype.markerDragged = function() {
+        if (this.options.reverseGeocoding) {
+          return this.reverseGeocode(this.marker.getPosition());
+        } else {
+          if (this.lastResult) {
+            this.lastResult.setLatLng(this.marker.getPosition().lat(), this.marker.getPosition().lng());
+          } else {
+            this.lastResult = new AddressPickerResult({
+              geometry: {
+                location: this.marker.getPosition()
+              }
+            });
           }
-        };
-      })(this));
-    };
+          return $(this).trigger('addresspicker:selected', this.lastResult);
+        }
+      };
 
-    AddressPicker.prototype.getGMap = function() {
-      return this.map;
-    };
+      AddressPicker.prototype.reverseGeocode = function(position) {
+        if (this.geocoder == null) {
+          this.geocoder = new google.maps.Geocoder();
+        }
+        return this.geocoder.geocode({
+          location: position
+        }, (function(_this) {
+          return function(results) {
+            if (results && results.length > 0) {
+              _this.lastResult = new AddressPickerResult(results[0], true);
+              return $(_this).trigger('addresspicker:selected', _this.lastResult);
+            }
+          };
+        })(this));
+      };
 
-    AddressPicker.prototype.getGMarker = function() {
-      return this.marker;
-    };
+      AddressPicker.prototype.getGMap = function() {
+        return this.map;
+      };
 
-    return AddressPicker;
+      AddressPicker.prototype.getGMarker = function() {
+        return this.marker;
+      };
 
-  })(Bloodhound);
+      return AddressPicker;
+
+    })(Bloodhound);
+  })(jQuery);
 
 }).call(this);
--- src/typeahead-addresspicker.coffee
+++ src/typeahead-addresspicker.coffee
@@ -1,135 +1,136 @@
-class @AddressPickerResult
-  constructor: (@placeResult, @fromReverseGeocoding = false) ->
-    @latitude = @placeResult.geometry.location.lat()
-    @longitude = @placeResult.geometry.location.lng()
+(($) ->
+  class @AddressPickerResult
+    constructor: (@placeResult, @fromReverseGeocoding = false) ->
+      @latitude = @placeResult.geometry.location.lat()
+      @longitude = @placeResult.geometry.location.lng()
 
-  addressTypes: ->
-    types = []
-    for component in @addressComponents()
-      for type in component.types
-        types.push(type) if types.indexOf(type) == -1
-    types
+    addressTypes: ->
+      types = []
+      for component in @addressComponents()
+        for type in component.types
+          types.push(type) if types.indexOf(type) == -1
+      types
 
-  addressComponents: ->
-    @placeResult.address_components || []
+    addressComponents: ->
+      @placeResult.address_components || []
 
-  address: ->
-    @placeResult.formatted_address
+    address: ->
+      @placeResult.formatted_address
 
-  nameForType: (type, shortName = false) ->
-    for component in @addressComponents()
-      return (if shortName then component.short_name else component.long_name) if component.types.indexOf(type) != -1
-    null
+    nameForType: (type, shortName = false) ->
+      for component in @addressComponents()
+        return (if shortName then component.short_name else component.long_name) if component.types.indexOf(type) != -1
+      null
 
-  lat: ->
-    @latitude
+    lat: ->
+      @latitude
 
-  lng: ->
-    @longitude
+    lng: ->
+      @longitude
 
-  setLatLng: (@latitude, @longitude) ->
+    setLatLng: (@latitude, @longitude) ->
 
 
-  isAccurate: ->
-    ! @placeResult.geometry.viewport
+    isAccurate: ->
+      ! @placeResult.geometry.viewport
 
-  isReverseGeocoding: ->
-    @fromReverseGeocoding
+    isReverseGeocoding: ->
+      @fromReverseGeocoding
 
-class @AddressPicker extends Bloodhound
-  constructor: (options = {})->
-    @options = $.extend
-      local: []
-      datumTokenizer: (d) -> Bloodhound.tokenizers.whitespace(d.num)
-      queryTokenizer: Bloodhound.tokenizers.whitespace
-      autocompleteService: {types: ["geocode"]}
-      zoomForLocation: 16
-      reverseGeocoding: false
-    , options
-    super(@options)
+  class @AddressPicker extends Bloodhound
+    constructor: (options = {})->
+      @options = $.extend
+        local: []
+        datumTokenizer: (d) -> Bloodhound.tokenizers.whitespace(d.num)
+        queryTokenizer: Bloodhound.tokenizers.whitespace
+        autocompleteService: {types: ["geocode"]}
+        zoomForLocation: 16
+        reverseGeocoding: false
+      , options
+      super(@options)
 
-    if @options.map
-      @initMap()
+      if @options.map
+        @initMap()
 
-    # Create a PlacesService on a fake DOM element
-    @placeService = new google.maps.places.PlacesService(document.createElement('div'))
+      # Create a PlacesService on a fake DOM element
+      @placeService = new google.maps.places.PlacesService(document.createElement('div'))
 
-  # Binds typeahead:selected and typeahead:cursorchanged event to @updateMap
-  bindDefaultTypeaheadEvent: (typeahead) ->
-    typeahead.bind("typeahead:selected", @updateMap)
-    typeahead.bind("typeahead:cursorchanged", @updateMap)
+    # Binds typeahead:selected and typeahead:cursorchanged event to @updateMap
+    bindDefaultTypeaheadEvent: (typeahead) ->
+      typeahead.bind("typeahead:selected", @updateMap)
+      typeahead.bind("typeahead:cursorchanged", @updateMap)
 
-  # Inits google map to display selected address from autocomplete
-  initMap: ->
-    if @options?.map?.gmap
-      @map = @options.map.gmap
-    else
-      @mapOptions = $.extend
-        zoom: 3
-        center: new google.maps.LatLng(0, 0)
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-        boundsForLocation: @updateBoundsForPlace
-      , @options.map
-      @map = new google.maps.Map($(@mapOptions.id)[0], @mapOptions)
-    @lastResult = null
-
-    # Create a hidden marker to display selected address
-    markerOptions = $.extend
-      draggable: true
-      visible: false
-      position: @map.getCenter()
-      map: @map
-    , @options.marker || {}
-    @marker = new google.maps.Marker(markerOptions)
-    if markerOptions.draggable
-      google.maps.event.addListener(@marker, 'dragend', @markerDragged)
-
-  # Overrides Bloodhound#get  to send request to google maps autocomplete service
-  get: (query, cb) ->
-    service = new google.maps.places.AutocompleteService()
-    @options.autocompleteService.input = query
-    service.getPlacePredictions @options.autocompleteService, (predictions) ->
-      data = (suggestion for suggestion in predictions)
-      cb(data)
-
-  # Callback for typeahead events like typeahead:selected or typeahead:cursorchanged
-  # to update marker position and map center/zoom for a specific Google Map place
-  updateMap: (event, place) =>
-    # Send place reference to place service to get geographic information
-    @placeService.getDetails place, (response) =>
-      @lastResult = new AddressPickerResult(response)
-      if @marker
-        @marker.setPosition(response.geometry.location)
-        @marker.setVisible(true)
-      if @map
-        @mapOptions?.boundsForLocation(response)
-      $(this).trigger('addresspicker:selected', @lastResult)
-
-  updateBoundsForPlace: (response) =>
-    if response.geometry.viewport
-      @map.fitBounds(response.geometry.viewport)
-    else
-      @map.setCenter(response.geometry.location)
-      @map.setZoom(@options.zoomForLocation)
-
-  markerDragged: () =>
-    if @options.reverseGeocoding
-      @reverseGeocode(@marker.getPosition())
-    else
-      if @lastResult
-        @lastResult.setLatLng(@marker.getPosition().lat(), @marker.getPosition().lng())
+    # Inits google map to display selected address from autocomplete
+    initMap: ->
+      if @options?.map?.gmap
+        @map = @options.map.gmap
       else
-        @lastResult = new AddressPickerResult(geometry: {location: @marker.getPosition()})
-      $(this).trigger('addresspicker:selected', @lastResult)
+        @mapOptions = $.extend
+          zoom: 3
+          center: new google.maps.LatLng(0, 0)
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+          boundsForLocation: @updateBoundsForPlace
+        , @options.map
+        @map = new google.maps.Map($(@mapOptions.id)[0], @mapOptions)
+      @lastResult = null
 
-  reverseGeocode: (position) ->
-    @geocoder ?= new google.maps.Geocoder()
-    @geocoder.geocode location: position, (results) =>
-      if results && results.length > 0
-        @lastResult = new AddressPickerResult(results[0], true)
+      # Create a hidden marker to display selected address
+      markerOptions = $.extend
+        draggable: true
+        visible: false
+        position: @map.getCenter()
+        map: @map
+      , @options.marker || {}
+      @marker = new google.maps.Marker(markerOptions)
+      if markerOptions.draggable
+        google.maps.event.addListener(@marker, 'dragend', @markerDragged)
+
+    # Overrides Bloodhound#get  to send request to google maps autocomplete service
+    get: (query, cb) ->
+      service = new google.maps.places.AutocompleteService()
+      @options.autocompleteService.input = query
+      service.getPlacePredictions @options.autocompleteService, (predictions) ->
+        data = (suggestion for suggestion in predictions)
+        cb(data)
+
+    # Callback for typeahead events like typeahead:selected or typeahead:cursorchanged
+    # to update marker position and map center/zoom for a specific Google Map place
+    updateMap: (event, place) =>
+      # Send place reference to place service to get geographic information
+      @placeService.getDetails place, (response) =>
+        @lastResult = new AddressPickerResult(response)
+        if @marker
+          @marker.setPosition(response.geometry.location)
+          @marker.setVisible(true)
+        if @map
+          @mapOptions?.boundsForLocation(response)
         $(this).trigger('addresspicker:selected', @lastResult)
 
-  # Attr accessor
-  getGMap: -> @map
-  getGMarker: -> @marker
+    updateBoundsForPlace: (response) =>
+      if response.geometry.viewport
+        @map.fitBounds(response.geometry.viewport)
+      else
+        @map.setCenter(response.geometry.location)
+        @map.setZoom(@options.zoomForLocation)
 
+    markerDragged: () =>
+      if @options.reverseGeocoding
+        @reverseGeocode(@marker.getPosition())
+      else
+        if @lastResult
+          @lastResult.setLatLng(@marker.getPosition().lat(), @marker.getPosition().lng())
+        else
+          @lastResult = new AddressPickerResult(geometry: {location: @marker.getPosition()})
+        $(this).trigger('addresspicker:selected', @lastResult)
+
+    reverseGeocode: (position) ->
+      @geocoder ?= new google.maps.Geocoder()
+      @geocoder.geocode location: position, (results) =>
+        if results && results.length > 0
+          @lastResult = new AddressPickerResult(results[0], true)
+          $(this).trigger('addresspicker:selected', @lastResult)
+
+    # Attr accessor
+    getGMap: -> @map
+    getGMarker: -> @marker
+)(jQuery)
